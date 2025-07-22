281a31bfdfda9bab3fbdca6501d26b84377eb736
Yui100901 <973800960@qq.com>
2025-07-22 10:43:08 +0800

    创建设备查询

---
+++ persistence/mapper/device_mapper.go
@@ -0,0 +1,15 @@
+package mapper
+
+import (
+	"nexus-core/persistence/base"
+	"nexus-core/persistence/model"
+)
+
+//
+// @Author yfy2001
+// @Date 2025/7/22 10 20
+//
+
+type DeviceMapper struct {
+	bm *base.Mapper[model.Device]
+}
