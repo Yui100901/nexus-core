8eb739d07af8fd44eed977d09e5259a904feaa77
Yui100901 <973800960@qq.com>
2025-07-23 13:30:33 +0800

    设备创建接口移动位置

--- api/device/cmd.go
+++ api/device/cmd.go
@@ -1,28 +1,14 @@
-package api
+package device
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
 	"nexus-core/domain/entity"
-	"nexus-core/service"
 	"time"
 )
 
 //
 // @Author yfy2001
-// @Date 2025/7/22 09 57
+// @Date 2025/7/23 13 28
 //
-
-type deviceController struct {
-	ds *service.DeviceService
-}
-
-func (dc *deviceController) RouteRegister(r *gin.RouterGroup) {
-	users := r.Group("/device")
-	{
-		users.GET("/create", dc.create)
-	}
-}
 
 type CreateDeviceCommand struct {
 	ID            string `json:"id"`            //id
@@ -50,18 +36,4 @@ func (c *CreateDeviceCommand) ToDomain() *entity.Device {
 			Status:        0,
 		},
 	}
-}
-
-func (dc *deviceController) create(ctx *gin.Context) {
-
-	var cmd CreateDeviceCommand
-	if err := ctx.ShouldBindJSON(&cmd); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	}
-	e := cmd.ToDomain()
-	dc.ds.CreateDevice(e)
-}
-
-func (dc *deviceController) createWithAuth(c *gin.Context) {
-
 }
