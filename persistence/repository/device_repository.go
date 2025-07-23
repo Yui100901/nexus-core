1c1303918175cef3132c09ef9ad10ba56f614c78
Yui100901 <973800960@qq.com>
2025-07-23 09:14:17 +0800

    添加转换方法

--- persistence/repository/device_repository.go
+++ persistence/repository/device_repository.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"nexus-core/domain/entity"
+	"nexus-core/persistence/converter"
 	"nexus-core/persistence/mapper"
 )
 
@@ -12,4 +14,18 @@ import (
 type DeviceRepository struct {
 	dm *mapper.DeviceMapper
 	am *mapper.AuthMapper
+	dc *converter.DeviceConverter
+}
+
+func NewDeviceRepository() *DeviceRepository {
+	return &DeviceRepository{
+		dm: mapper.NewDeviceMapper(),
+		am: mapper.NewAuthMapper(),
+	}
+}
+
+func (dr *DeviceRepository) SaveOrUpdate(e *entity.Device) {
+	d, a := dr.dc.ToPersistence(e)
+	dr.dm.SaveOrUpdate(d)
+	dr.am.SaveOrUpdate(a)
 }
