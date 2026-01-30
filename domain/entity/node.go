53b03d9e2bb16cbc8a1e8fa41642d132a3303aa4
Yui100901 <973800960@qq.com>
2026-01-30 16:35:45 +0800

    删除耦合的binding

--- domain/entity/node.go
+++ domain/entity/node.go
@@ -10,10 +10,9 @@ import "fmt"
 // Node 表示用户环境的抽象节点
 // 代表一台物理设备、虚拟设备或其他运行环境
 type Node struct {
-	ID         uint                 // 节点唯一标识符
-	DeviceCode string               // 设备唯一识别码，用于区分不同设备
-	MetaInfo   *string              // 设备元信息，包含操作系统、版本等信息
-	Bindings   []NodeLicenseBinding // 与此节点关联的许可证绑定关系
+	ID         uint    // 节点唯一标识符
+	DeviceCode string  // 设备唯一识别码，用于区分不同设备
+	MetaInfo   *string // 设备元信息，包含操作系统、版本等信息
 }
 
 // NewNode 工厂方法
@@ -26,7 +25,6 @@ func NewNode(deviceCode string, metaInfo *string) (*Node, error) {
 	node := &Node{
 		DeviceCode: deviceCode,
 		MetaInfo:   metaInfo,
-		Bindings:   []NodeLicenseBinding{}, // 初始化为空列表
 	}
 
 	return node, nil
