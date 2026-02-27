ab551d59b33be1f2d5edc81a93a932d438afb519
Yui100901 <973800960@qq.com>
2026-02-27 15:30:14 +0800

    分离gin中间件

--- api/engine.go
+++ api/engine.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"fmt"
-	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -20,27 +19,11 @@ import (
 
 var WebEngine *gin.Engine
 
-// Cors 跨域配置中间件
-// 配置CORS头部信息，允许跨域请求
-func Cors() gin.HandlerFunc {
-	return func(context *gin.Context) {
-		method := context.Request.Method
-		context.Header("Access-Control-Allow-Origin", "*")
-		context.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token, x-token")
-		context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH, PUT")
-		context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-		context.Header("Access-Control-Allow-Credentials", "true")
-		if method == "OPTIONS" {
-			context.AbortWithStatus(http.StatusNoContent)
-		}
-	}
-}
-
 // NewServer 创建并配置Gin服务器引擎
 // 包括跨域配置、请求ID中间件和简单日志中间件
 func NewServer() *gin.Engine {
 	r := gin.Default()
-	r.Use(Cors())
+	r.Use(CorsMiddleware())
 	// request id middleware
 	r.Use(func(c *gin.Context) {
 		id := uuid.New().String()
---
+++ api/middleware.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+//
+// @Author yfy2001
+// @Date 2026/2/27 15 23
+//
+
+// CorsMiddleware 跨域配置中间件
+// 配置CORS头部信息，允许跨域请求
+func CorsMiddleware() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		method := context.Request.Method
+		context.Header("Access-Control-Allow-Origin", "*")
+		context.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token, x-token")
+		context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH, PUT")
+		context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		context.Header("Access-Control-Allow-Credentials", "true")
+		if method == "OPTIONS" {
+			context.AbortWithStatus(http.StatusNoContent)
+		}
+		context.Next()
+	}
+}
