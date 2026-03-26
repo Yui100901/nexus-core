c5fd05a3542cd81536ebee0ae6ca27186e237e42
Yui100901 <973800960@qq.com>
2026-03-26 10:30:04 +0800

    增加部分辅助方法

--- global/global.go
+++ global/global.go
@@ -1,6 +1,14 @@
 package global
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 //
 // @Author yfy2001
@@ -10,3 +18,19 @@ import "gorm.io/gorm"
 var (
 	DB *gorm.DB
 )
+
+func GetLogger(sc *gin.Context) *log.Logger {
+	traceID := sc.GetHeader("X-Trace-ID")
+	if traceID == "" {
+		traceID = uuid.New().String()
+	}
+	requestID := sc.GetHeader("X-Request-ID")
+	if requestID == "" {
+		requestID = uuid.New().String()
+	}
+	method := sc.Request.Method
+	path := sc.Request.URL.Path
+	prefix := fmt.Sprintf("[TraceID:%s] [RequestID:%s] [%s %s] ", traceID, requestID, method, path)
+	logger := log.New(os.Stdout, prefix, log.LstdFlags)
+	return logger
+}
