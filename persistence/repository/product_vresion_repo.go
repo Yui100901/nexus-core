e3f519acf3ab7fa670cf4ae9d371cd4ec405b1d7
Yui100901 <973800960@qq.com>
2026-03-26 16:31:57 +0800

    加入产品版本的一些方法

--- persistence/repository/product_vresion_repo.go
+++ persistence/repository/product_vresion_repo.go
@@ -1,6 +1,45 @@
 package repository
 
+import (
+	"context"
+	"nexus-core/persistence/model"
+
+	"gorm.io/gorm"
+)
+
 //
 // @Author yfy2001
 // @Date 2026/3/26 15 59
 //
+
+type ProductVersionRepository struct {
+}
+
+func NewProductVersionRepository() *ProductVersionRepository {
+	return &ProductVersionRepository{}
+}
+
+func (r *ProductVersionRepository) Create(ctx context.Context, db *gorm.DB, productVersion *model.ProductVersion) error {
+	if err := gorm.G[model.ProductVersion](db).Create(ctx, productVersion); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *ProductVersionRepository) GetByID(ctx context.Context,
+	db *gorm.DB, id string) (*model.ProductVersion, error) {
+	m, err := GetOneByUniqueColumn[model.ProductVersion](ctx, db, "id", id)
+	if err != nil {
+		return nil, err
+	}
+	if m == nil {
+		return nil, nil
+	}
+
+	return m, nil
+}
+
+func (r *ProductVersionRepository) ListByProductID(ctx context.Context,
+	db *gorm.DB, productID string) ([]model.ProductVersion, error) {
+	return FindByColumn[model.ProductVersion](ctx, db, "product_id", productID)
+}
